65a3d9bc4846aad456d70b25f594a1e1a1085a16
Zcman03 <zct00003@mix.wvu.edu>
2024-02-29 16:29:37 -0500

    Second Part of Lab

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "boostrap/dist/css/bootstrap.css";
+import MyNavbar from './Navbar';
+import CardComponent from './Card';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <NavbarComponent />
+    <CardComponent />
   </React.StrictMode>
 );
 
