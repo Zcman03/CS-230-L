a31bd3c7dfd18fb1e353b0b9c41c14e4683d8e01
Zcman03 <zct00003@mix.wvu.edu>
2024-02-29 16:55:53 -0500

    Fixes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "boostrap/dist/css/bootstrap.css";
-import MyNavbar from './Navbar';
+import "bootstrap/dist/css/bootstrap.css";
+import NavbarComponent from './Navbar';
 import CardComponent from './Card';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
