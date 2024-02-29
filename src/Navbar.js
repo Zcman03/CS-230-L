374a09bd12b6360c4699f4a50e256f7d0dc40557
Zcman03 <zct00003@mix.wvu.edu>
2024-02-29 16:39:42 -0500

    Second Part of Lab

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 
-const MyNavbar = () => {
+const NavbarComponent = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Navbar.Brand href="#home">Your Brand</Navbar.Brand>
