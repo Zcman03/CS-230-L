885962061d243936594542ef49ebc7125a8b0603
Zcman03 <zct00003@mix.wvu.edu>
2024-03-02 16:08:14 -0500

    Color Cards

--- src/Card.js
+++ src/Card.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
+
 function CardComponent() {
   return (
     <div class="card-container d-flex justify-content-center">
-    
-    <div className="card" style={{ width: '18rem', margin: '5px'}}>
+      <div className="card" style={{ width: '18rem', margin: '5px', backgroundColor:'blue'}}>
         <div className="card-body" style={{}}>
-          <h5 className="card-title" class="card-container d-flex justify-content-center" >Card One</h5>
-          <p className="card-text">
+          <h5 className="card-title" class="card-container d-flex justify-content-center" style={{color:'white'}}>Card One</h5>
+          <p className="card-text" style={{color:'white'}}>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </p>
         </div>
       </div>
 
-      <div className="card" style={{ width: '18rem', margin: '5px'}}>
+      <div className="card" style={{ width: '18rem', margin: '5px', backgroundColor:'red'}}>
         <div className="card-body" style={{}}>
-          <h5 className="card-title" class="card-container d-flex justify-content-center" >Card Two</h5>
-          <p className="card-text">
+          <h5 className="card-title" class="card-container d-flex justify-content-center" style={{color:'white'}}>Card Two</h5>
+          <p className="card-text" style={{color:'white'}}>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </p>
@@ -25,10 +25,10 @@ function CardComponent() {
       </div>
 
       
-      <div className="card" style={{ width: '18rem', margin: '5px'}}>
+      <div className="card" style={{ width: '18rem', margin: '5px', backgroundColor:'green'}}>
         <div className="card-body" style={{}}>
-          <h5 className="card-title" class="card-container d-flex justify-content-center" >Card Three</h5>
-          <p className="card-text">
+          <h5 className="card-title" class="card-container d-flex justify-content-center" style={{color:'white'}}>Card Three</h5>
+          <p className="card-text" style={{color:'white'}}>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </p>
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import NavbarComponent from './Navbar';
 import CardComponent from './Card';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
