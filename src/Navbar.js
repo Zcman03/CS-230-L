0862ac56c3d681990bf9c9848595613f52eb7a33
Zcman03 <zct00003@mix.wvu.edu>
2024-03-03 13:57:56 -0500

    added search bar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { Navbar, Nav, Form, FormControl, Button } from 'react'
+
 function NavbarComponent() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -27,6 +30,13 @@ function NavbarComponent() {
                 </div>
               </li>
             </ul>
+            <div class="m-auto"> </div>
+            <ul class="d-flex justify-content-center">
+              <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+              </form>
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+           </ul>
           </div>
         </div>
       </nav>
